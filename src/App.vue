433e961c240c6ed80de28004daf567b6331da9d3
Sheng-Liang Slogar <slogar.sheng@gmail.com>
2018-11-23 01:04:16 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Can I Text 911?</h1>
+        <p>
+            Coming Soon
+        </p>
+        <p>
+            <Button @click="handleRegistry">View FCC Registry</Button>
+        </p>
+        <p>
+            <small>Questions? Email support@shengslogar.com</small>
+        </p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Button from "@/components/Button";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        methods: {
+            handleRegistry() {
+                window.open('https://www.fcc.gov/files/text-911-master-psap-registryxlsx');
+            }
+        },
+        components: {Button}
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        box-sizing: border-box;
+    }
+
+    body {
+        font: 16px Oxygen, sans-serif;
+        height: 100vh;
+        margin: 0;
+        cursor: default;
+        color: #444;
+    }
+
+    h1 {
+        font-size: 4rem;
+        margin: 0;
+        color: #000;
+    }
+
+    #app {
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        display: flex;
+        min-height: 100%;
+        width: 100%;
+    }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="button"
+         @click="$emit('click')">
+        <slot/>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Button'
+    };
+</script>
+<style>
+    .button {
+        border-radius: 2rem;
+        cursor: pointer;
+        background: #1660be;
+        color: #fff;
+        padding: 0 2rem;
+        height: 3rem;
+        display: flex;
+        white-space: nowrap;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
